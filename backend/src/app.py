1bd40d51efa82ec1b7cca9cc764df6eaf9f14e76
Banias Baabe <banias@hotmail.de>
2023-12-17 22:28:47 +0100

    Fix error with strings within llm output

--- backend/src/app.py
+++ backend/src/app.py
@@ -8,6 +8,7 @@ from langchain import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
 from notion_client import Client
+import ast
 from pydantic import BaseModel
 
 FRONTEND_ENDPOINT = dotenv_values(".env").get("FRONTEND_ENDPOINT", None)
@@ -53,6 +54,7 @@ class GetTemplates(BaseModel):
 @app.post("/create_template")
 async def create_template(data: TemplateCreate):
     notion = Client(auth=data.notionKey)
+    database_id = None
     if not data.databaseId:
         properties = {
             "Title": {"title": {}},
@@ -91,13 +93,13 @@ async def create_template(data: TemplateCreate):
     templatizing_prompt = """
     You are GPT-Template, a program that turns LinkedIn Posts into perfectly usable templates. \
     A template is a piece of content with the right formatting & post structure, with bracket like "[]" filled with the best indication for the writer \
-    to make it its own piece of text. Here is an example. The original LinkedIn post between '':\n'The 9 to 5 is getting pummeled.\nThe great resignation is growing faster than ever.\nAnd I love it.\nWhy?\nBecause the workforce is tired...'\
-    The template GPT-Template should provide between '':'The [issue/topic] is [massive change]\nThe [trend] is [intensifying].\nAnd I [strong emotion] it.\nWhy?\nBecause [target audience] are [strong negative emotion].'\
-    Here's another LinkedIn post example between '': 'I quit my job.\nIt was the biggest salary I ever made in my life.\nMy personal income went to $0.\nI threw away 66% of my belongings.'\
-    Here's what GPT-Template should answer between '':'I [significant decision or action].\nIt was the [notable achievement] in my [context].\nMy [personal consequence or change].\nI [action taken] of my [posessions or attachments].'.\n\
+    to make it its own piece of text. Here is an example. The original LinkedIn post between '''''':\n'''The 9 to 5 is getting pummeled.\nThe great resignation is growing faster than ever.\nAnd I love it.\nWhy?\nBecause the workforce is tired...'''\
+    The template GPT-Template should provide between '''''':'''The [issue/topic] is [massive change]\nThe [trend] is [intensifying].\nAnd I [strong emotion] it.\nWhy?\nBecause [target audience] are [strong negative emotion].'''\
+    Here's another LinkedIn post example between '''''': '''I quit my job.\nIt was the biggest salary I ever made in my life.\nMy personal income went to $0.\nI threw away 66% of my belongings.'''\
+    Here's what GPT-Template should answer between '''''':'''I [significant decision or action].\nIt was the [notable achievement] in my [context].\nMy [personal consequence or change].\nI [action taken] of my [posessions or attachments].'''.\n\
     Now, I will give you a LinkedIn posts. I want you to generate only the reusable template. The template should be generic and used on any topic. The template should use the same formatting, that means the same spaces and enters.\
     I want it to look less like of a post but more like a template anyone could use. The output should have the following \
-    dictionary format in minimized form (no spaces, ideally one line): {{"title": "Short title of template", "post": "The template you made"}} Please do your best, this is important to my career. \
+    dictionary format in minimized form (no spaces, ideally one line): {{"title": '''Short title of template''', "post": '''The template you made'''}} Please do your best, this is important to my career. \
     I'm going to tip you $200 for a perfect response.\
     This is the LinkedIn post: '{LINKEDIN_POST}'.
     """
@@ -109,8 +111,8 @@ async def create_template(data: TemplateCreate):
     llm_chain = LLMChain(llm=llm, prompt=templatizing_prompt_template)
 
     template = llm_chain(data.text)["text"]
-
-    template = eval(template)
+    
+    template = ast.literal_eval(template)
 
     parent = {"database_id": database_id if database_id else data.databaseId}
     properties = {
@@ -185,11 +187,11 @@ async def generate_posts(data: GeneratePosts):
     )
 
     creating_posts_prompt = """
-    You are a viral Content creator. You will take a template and a topic, and generate posts from it. Here is the template between '': \
-    '{TEMPLATE}' \
+    You are a viral Content creator. You will take a template and a topic, and generate posts from it. Here is the template between '''''': \
+    '''{TEMPLATE}''' \
     Based on this template, generate {NUMBER_OF_POSTS} different posts where you will only fill in the brackets. The posts should be around the topics {TOPICS}.\
     Please use the following output format where you will output a list of dictionaries in minimized form (no spaces, ideally one line):\
-    [{{"title": "Short Title of the post", "post": "The post you made"}}, {{"title": "Another short title", "post": "Another post you made"}}] \
+    [{{"title": '''Short Title of the post''', "post": '''The post you made'''}}, {{"title": '''Another short title''', "post": '''Another post you made'''}}] \
     Please do your best, this is important to my career. I'm going to tip you $200 for a perfect response.
     """
 
@@ -211,7 +213,7 @@ async def generate_posts(data: GeneratePosts):
         }
     )["text"]
 
-    posts = eval(posts)
+    posts = ast.literal_eval(posts)
 
     for post in posts:
         parent = {"database_id": data.databaseId}
